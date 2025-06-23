e944d3f57b0a2be2acd949860a26ad9cf6925ec2
ali <ali@gmail.com>
2025-06-23 23:17:50 +0100

    frontmarchework

--- src/main/java/com/example/studentmanagement/controllers/StudentController.java
+++ src/main/java/com/example/studentmanagement/controllers/StudentController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
-@CrossOrigin(origins = "*")
+@CrossOrigin(origins = "http://localhost:4200")
 @RestController
 @RequestMapping("/api/students")
 public class StudentController {
